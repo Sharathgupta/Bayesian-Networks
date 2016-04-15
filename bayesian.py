cc461a8cba2aaaeb544bb385264fd470633621c2
Sharathgupta <palashiv@usc.edu>
2016-04-15 16:03:41 -0700

    15apr 2016 4pm

--- bayesian.py
+++ bayesian.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import copy
+import itertools
 
 fp = open(sys.argv[-1],'r')
 line = fp.readlines()
@@ -37,7 +39,10 @@ while (line[i].strip() != "******"):
 	dictionary = {}
 	dictionary['query'] = dict_value
 	dictionary['type'] = query_type
-	dictionary['given'] = dict_value2
+	if pipe != -1:
+		dictionary['given'] = dict_value2
+	else:
+		dictionary['given'] = []
 	main_list.append(dictionary)
 	i = i + 1
 
@@ -63,22 +68,110 @@ def network_build(temp):
 		if parent:
 			index = each_value.find(" ")
 			prob_val = each_value[:index]
-			prob_type = each_value[index+1:]
+			prob_type = each_value[index+1:].replace(" ", "")
 			internal_dict2[prob_type] = prob_val
 			internal_dict['cond_prob'] = internal_dict2
 			internal_dict['abs_prob'] = ''
 		else:
 			internal_dict['abs_prob'] = each_value.strip()
-			internal_dict['cond_prob'] = each_value.strip()
-	net_dict[key] = internal_dict
+			internal_dict['cond_prob'] = {}
+	net_dict[key.strip()] = internal_dict
 
 
-while(i < len(line) and line[i].strip() != "******"):
+while(i < len(line)):
 	temp = []
 	while i < len(line) and line[i].strip() != "***":
+		if line[i].strip() == "******":
+			break
 		temp.append(line[i].strip())
 		i = i + 1
 	network_build(temp)
 	i = i + 1
 
-print net_dict
+#topological sorting
+variables = list(net_dict.keys())
+variables.sort()
+s = set()   # used to mark variables
+l = []
+while len(s) < len(variables):
+	for v in variables:
+		if v not in s and all(x in s for x in net_dict[v]['parent']):
+			s.add(v)
+			l.append(v)
+
+print "network_build: ", net_dict
+print
+print "Query list: ", main_list
+print
+print "SORTED VARS: ", l
+
+def get_value(y, lists):
+	if net_dict[y]['abs_prob'] == "":
+		all_parents = ""
+		for parent in net_dict[y]['parent']:
+			all_parents = all_parents + lists[parent]
+		prob_value = float(net_dict[y]['cond_prob'][all_parents])
+		if lists[y] == "-":
+			return 1 - prob_value
+		else:
+			return prob_value
+	else:
+		prob_value = float(net_dict[y]['abs_prob'])
+		if lists[y] == "+":
+			return prob_value
+		else:
+			return (1 - prob_value)
+
+def permutation_generate(num):
+	pset = set()
+	for count in itertools.combinations_with_replacement(['+', '-'], num):
+		for inner_count in itertools.permutations(count):
+			pset.add(inner_count)
+	return list(pset)
+
+def enumeration_ask(query):
+	X = query["query"]
+	e = query["given"]
+	bn = copy.deepcopy(l)	
+	X_dict = {}
+	e_dict = {}
+	ex_dict = {}
+	X_dict = dict(X)
+	if e:
+		e_dict = dict(e)
+	ex_dict = dict(e_dict)
+	ex_dict.update(X_dict)
+	qx = []
+	qe = enumerate_all(l, ex_dict)
+	
+	permutations = permutation_generate(len(X_dict))
+	
+	for each_perm in permutations:
+		x = 0
+		for each_X in X_dict.keys():
+			ex_dict[each_X] = each_perm[x]
+			x = x + 1
+		qx.append(enumerate_all(l, ex_dict))
+	return qe/sum(qx)
+
+def enumerate_all(svars, e):
+	if len(svars) == 0:
+		return 1.0
+	Y = svars[0]
+	res = 1.0
+	e_dict = dict(e)
+	probs = []
+	if Y not in e:
+		for value in ['+', '-']:
+			e_dict[Y] = value
+			value = get_value(Y, e_dict)
+			probs.append(value * enumerate_all(svars[1:], e_dict))
+		res = sum(probs)
+	else:
+		value = get_value(Y, e_dict)
+		res = value * enumerate_all(svars[1:], e)
+
+	return res
+
+for each_query in main_list:
+	print enumeration_ask(each_query)
